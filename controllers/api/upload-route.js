ba6d2504beb1d14a3f88d48b8c788b28e02d5cdd
Santiago Palacios <santipalacios2002@gmail.com>
2021-07-07 16:58:52 -0400

    got upload to work

--- controllers/api/upload-route.js
+++ controllers/api/upload-route.js
@@ -1,11 +1,11 @@
 /* eslint-disable prefer-const */
 /* eslint-disable no-undef */
 const fs = require('fs');
-const mysql = require('mysql2');
 const csv = require('fast-csv');
 require('dotenv').config();
 const multer = require('multer');
 const router = require('express').Router();
+const { Guest } = require('../../models');
 
 global.__basedir = __dirname;
 
@@ -14,7 +14,6 @@ const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, __basedir + '/uploads/');
     console.log('__basedir:', __basedir);
-    //    console.log('__basedir + '/uploads/':', __basedir + '/uploads/')
   },
   filename: (req, file, cb) => {
     cb(null, file.fieldname + '-' + Date.now() + '-' + file.originalname);
@@ -23,27 +22,9 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-router.get('/', (req, res) => {
-  // find all events
-  try {
-    res.status(200).json({ message: 'api route upload file' });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 // -> Express Upload RestAPIs
-router.post('/uploadfile', upload.single('uploadfile'), (req, res) => {
-  importCsvData2MySQL(__basedir + '/uploads/' + req.file.filename);
-  res.json({
-    msg: 'File uploaded/import successfully!',
-    file: req.file
-  });
-});
-
-// -> Import CSV File to MySQL database
-function importCsvData2MySQL (filePath) {
-  let stream = fs.createReadStream(filePath);
+router.post('/uploadfile/:eventId', upload.single('uploadfile'), (req, res) => {
+  let stream = fs.createReadStream(__basedir + '/uploads/' + req.file.filename);
   let csvData = [];
 
   let csvStream = csv
@@ -55,56 +36,32 @@ function importCsvData2MySQL (filePath) {
       // Remove Header ROW
       csvData.shift();
 
-      console.log(csvData[0][1]);
-
-      for (let index = 0; index < csvData.length; index++) {
-        csvData[index][3] = '2';
-      }
-
-      // Create a connection to the database
-      const connector = () => {
-        if (process.env.JAWSDB_URL) {
-          return mysql.createConnection(process.env.JAWSDB_URL);
-        } else {
-          return mysql.createConnection({
-            host: 'localhost',
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME
-          });
-        }
-      };
-      const connection = connector();
-
-      // Open the MySQL connection
-      connection.connect((error) => {
-        if (error) {
-          console.error(error);
-        } else {
-          let query =
-            'INSERT INTO guest (firstName, lastName, tableNumber, eventId) VALUES ?';
-          connection.query(query, [csvData], (error, response) => {
-            console.log(error || response);
-          });
-        }
+      const guestData = csvData.map((guest) => {
+        return {
+          firstName: guest[0],
+          lastName: guest[1],
+          tableNumber: guest[2],
+          eventId: req.params.eventId
+        };
       });
+
+      Guest.bulkCreate(guestData)
+        .then(() => {
+          res.json({
+            msg: 'File uploaded/import successfully!',
+            file: req.file
+          });
+        })
+        .catch((err) => {
+          res.status(500).json(err);
+        });
 
       // delete file after saving to MySQL database
       // -> you can comment the statement to see the uploaded CSV file.
-      fs.unlinkSync(filePath);
+      fs.unlinkSync(__basedir + '/uploads/' + req.file.filename);
     });
 
   stream.pipe(csvStream);
-}
-
-// // Create a Server
-// let server = app.listen(3030, function () {
-
-//   let host = server.address().address
-//   let port = server.address().port
-
-//   console.log("App listening at http://%s:%s", host, port)
-
-// })
+});
 
 module.exports = router;
