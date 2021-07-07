78944155a4aac4cefcd202ecaab1249b60bbae40
Santiago Palacios <santipalacios2002@gmail.com>
2021-07-07 13:49:21 -0400

    added get event routes

--- controllers/api/events-routes.js
+++ controllers/api/events-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { Event, Guest } = require('../../models');
 
 // once Event table on mysql is created, here we need to import the event model
 // const Event = require('../../models/Event');  <== uncomment when event model is created
@@ -7,6 +8,36 @@ router.get('/', (req, res) => {
   // find all events
   try {
     res.status(200).json({ message: 'api route for all Events Data' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get all events by admin
+router.get('/:adminId', async (req, res) => {
+  try {
+    const eventData = await Event.findAll({
+      where: {
+        adminId: req.params.adminId
+      }
+    });
+    res.status(200).json(eventData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get an event by admin (including guests)
+router.get('/:adminId/:eventId', async (req, res) => {
+  try {
+    const eventData = await Event.findAll({
+      where: {
+        adminId: req.params.adminId,
+        eventId: req.params.eventId
+      },
+      include: { model: Guest }
+    });
+    res.status(200).json(eventData);
   } catch (err) {
     res.status(500).json(err);
   }
