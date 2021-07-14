785be7d3c7ff6bdc594fecf3298d48956a9e4fa4
Santiago Palacios <santipalacios2002@gmail.com>
2021-07-14 09:38:26 -0400

    eventname rendering in seating and upload views

--- public/js/eventheaders.js
+++ public/js/eventheaders.js
@@ -1,7 +1,7 @@
 const event = window.location.search.split('=');
 const eventId = event[1];
 let eventName = '';
-const renderEvent = document.getElementById('eventName');
+const renderEvent = document.getElementById('renderEventName');
 const requestOptions = {
   method: 'GET',
   redirect: 'follow'
@@ -10,9 +10,7 @@ const requestOptions = {
 fetch(`../api/admin/eventinfo/${eventId}`, requestOptions)
   .then((response) => response.text())
   .then((result) => {
-    console.log('result:', result);
     const myArray = result.split(',');
-    console.log('myArray:', myArray);
 
     eventName = myArray[2].substring(13, myArray[2].length - 1);
     renderEvent.innerHTML = `${eventName}`;
